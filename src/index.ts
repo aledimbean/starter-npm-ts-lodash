a85edeeaf25a944d699b9348b8b32c07568f016d
aledimbean <aledimbean@gmail.com>
2021-07-29 16:41:18 +0300

    type definitions test with lodash

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-let someVariable = 0;
+import _ from "lodash";
 
-console.log(someVariable);
- 
+console.log(_.add(3,4));
+
+console.log(_.dropRight([1,2,3,4,5], 2));
+
+console.log(_.dropRight(["cat", "dog","fish","blueberry","pineapple"], 1));
