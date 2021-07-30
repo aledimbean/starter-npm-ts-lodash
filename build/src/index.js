86a5fa958c2f88e22785f0680b9feb51fffb1bc3
aledimbean <aledimbean@gmail.com>
2021-07-30 17:11:28 +0300

    integrate jasmine, write first test, types issue

--- build/src/index.js
+++ build/src/index.js
@@ -7,3 +7,6 @@ var lodash_1 = __importDefault(require("lodash"));
 console.log(lodash_1.default.add(3, 4));
 console.log(lodash_1.default.dropRight([1, 2, 3, 4, 5], 2));
 console.log(lodash_1.default.dropRight(["cat", "dog", "fish", "blueberry", "pineapple"], 1));
+console.log(lodash_1.default.multiply(2, 5));
+var myFunc = function (num) { return (num * 5); };
+exports.default = myFunc;
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ console.log(_.dropRight([1,2,3,4,5], 2));
 console.log(_.dropRight(["cat", "dog","fish","blueberry","pineapple"], 1));
 
 console.log(_.multiply(2,5));
+
+const myFunc = (num:any) => ((num * 5) as any);
+
+export default myFunc;
